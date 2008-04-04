7fe050e5bc80041a41657e9244e87d3a80854dc2
Federico Mena Quintero <federico@gnu.org>
2008-04-04 23:39:16 +0000

    Checkpoint startup
    Signed-off-by: Federico Mena Quintero <federico@gnu.org>
    
    svn path=/trunk/; revision=409

--- main-menu/src/main-menu.c
+++ main-menu/src/main-menu.c
@@ -26,6 +26,7 @@
 #include <panel-applet.h>
 #include <string.h>
 
+#include "libslab-utils.h"
 #include "main-menu-ui.h"
 
 #include "main-menu-migration.h"
@@ -35,10 +36,17 @@ static gboolean main_menu_applet_init (PanelApplet *, const gchar *, gpointer);
 PANEL_APPLET_BONOBO_FACTORY ("OAFIID:GNOME_MainMenu_Factory", PANEL_TYPE_APPLET, "Main Menu", "0",
 	main_menu_applet_init, NULL);
 
+#define CHECKPOINT_CONFIG_BASENAME "main-menu-checkpoint.conf"
+#define CHECKPOINT_FILE_BASENAME "main-menu"
+
 static gboolean
 main_menu_applet_init (PanelApplet *applet, const gchar *iid, gpointer user_data)
 {
 	gchar *argv [1] = { "slab" };
+
+	libslab_checkpoint_init (CHECKPOINT_CONFIG_BASENAME, CHECKPOINT_FILE_BASENAME);
+
+	libslab_checkpoint ("Main-menu starts up");
 
 	if (strcmp (iid, "OAFIID:GNOME_MainMenu") != 0)
 		return FALSE;
@@ -58,13 +66,17 @@ main_menu_applet_init (PanelApplet *applet, const gchar *iid, gpointer user_data
 
 	gnome_program_init (PACKAGE, VERSION, LIBGNOMEUI_MODULE, 1, argv, NULL, NULL);
 
+	libslab_checkpoint ("Migrating old configurations");
 	migrate_system_gconf_to_bookmark_file    ();
 	migrate_user_apps_gconf_to_bookmark_file ();
 	migrate_showable_file_types              ();
 
+	libslab_checkpoint ("Creating user interface for whole applet");
 	main_menu_ui_new (applet);
 
+	libslab_checkpoint ("Showing all widgets in applet");
 	gtk_widget_show_all (GTK_WIDGET (applet));
 
+	libslab_checkpoint ("Finished initializing applet");
 	return TRUE;
 }
